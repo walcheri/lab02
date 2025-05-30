9d6f379d78ed87865d552b83c62a9279774fea2b
walcheri <vagzvrf@mail.ru>
2025-05-30 03:37:33 +0300

    deleted namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-using namespace std;
 int main(){
-	string name;
-	cout<<"enter ur name: ";
-	cin>>name;
-	cout<<"Hello, world from "<<name;
+	std::string name;
+	std::cout<<"enter ur name: ";
+	std::cin>>name;
+	std::cout<<"Hello, world from "<<name;
 	return 0;
 }
