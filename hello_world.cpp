eda58c7baabe445a266e220e8a109641726cd4e2
walcheri <vagzvrf@mail.ru>
2025-05-30 03:50:03 +0300

    new comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 int main(){
 	std::string name;
-	std::cout<<"enter ur name: ";
+	std::cout<<"enter ur name: "; //name
 	std::cin>>name;
-	std::cout<<"Hello, world from "<<name;
+	std::cout<<"Hello, world from "<<name; //print
 	return 0;
 }
